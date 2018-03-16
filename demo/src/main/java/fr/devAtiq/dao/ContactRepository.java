f9831328592c9df1b2bb1c132d46ec4710fd33aa
atiq <joseph1atiq@gmail.com>
2018-03-16 12:21:38 +0100

    add a method searchContact to ContactRepository, 	then modifie the API rest to take account this change

--- demo/src/main/java/fr/devAtiq/dao/ContactRepository.java
+++ demo/src/main/java/fr/devAtiq/dao/ContactRepository.java
@@ -2,12 +2,20 @@ package fr.devAtiq.dao;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import fr.devAtiq.entities.Contact;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 
-	//List<Contact> findByName(String name);
+	
+	//add a method to search for contacts
+	@Query("select c from Contact c where c.name like :x")
+	public Page<Contact> searchContact(@Param("x") String mc, Pageable pageable);
+	
 	
 }
--- demo/src/main/java/fr/devAtiq/web/ContactRestService.java
+++ demo/src/main/java/fr/devAtiq/web/ContactRestService.java
@@ -1,16 +1,20 @@
 package fr.devAtiq.web;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import antlr.collections.List;
 import fr.devAtiq.dao.ContactRepository;
 import fr.devAtiq.entities.Contact;
 
-//service rest 
+
 @RestController
 
 public class ContactRestService {
@@ -18,7 +22,48 @@ public class ContactRestService {
 	@Autowired
 	private ContactRepository contactRepository;
 	
+	
+	@RequestMapping(value="/contact", method=RequestMethod.GET)
+	public  java.util.List<Contact> getContact(){
+		return contactRepository.findAll();
+	}
+	@RequestMapping(value="/contact/{id}", method=RequestMethod.GET)
+	public  Contact getContact(@PathVariable Long id){
+		return contactRepository.findOne(id);
+	
+	}
+	@RequestMapping(value="/searchContact", method=RequestMethod.GET)
+	public  Page<Contact> searchContact(
+			@RequestParam(name="mc", defaultValue="") String mc, 
+			@RequestParam(name="page", defaultValue="0") int page, 
+			@RequestParam(name="size", defaultValue="5") int size){
+		return contactRepository.searchContact(mc, new PageRequest(page, size));
+	
+	}
+	//to add contact
+	@RequestMapping(value="/contact/", method=RequestMethod.POST)
+	public  Contact save(@RequestBody Contact c){
+		return contactRepository.save(c);
+	}
+	
+	@RequestMapping(value="/contact/{id}", method=RequestMethod.DELETE)
+	public boolean delete(@PathVariable Long id){
+		 contactRepository.delete(id);
+		 return true;
+		 
+	/*@RequestMapping(value="/contact/{id}", method=RequestMethod.PUT)
+ 
+	public Contact save(@PathVariable Long id, @RequestBody Contact c) {
+		c.setId(id);
+		
+		return  contactRepository.save(c);*/
+		
+	}
+			
+		 
+		
 
 	
 
 }
+
